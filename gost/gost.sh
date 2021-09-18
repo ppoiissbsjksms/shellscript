fc43f3d62058ae997ef7b490edb3d34ab273b064
myxuchangbin <gg18701605431@gmail.com>
2021-09-18 18:19:56 +0800

    修复cn下载及启动脚本优化

--- gost/gost.sh
+++ gost/gost.sh
@@ -13,34 +13,36 @@ if [ -n "$1" ]; then
         GITHUB_URL="hub.fastgit.org"
     fi
 fi
-
-URL="$(wget -qO- https://api.github.com/repos/ginuerzh/gost/releases/latest | grep -E "browser_download_url.*gost-linux-amd64" | cut -f4 -d\")"
+if [ ${CN} == 1 ]; then
+    URL="https://ghproxy.com/https://github.com$(wget -qO- "https://ghproxy.com/https://github.com/ginuerzh/gost/releases/latest" | grep -E "href=.*gost-linux-amd64" | awk -F "\"" '{print $2}')"
+else
+    URL="$(wget -qO- https://api.github.com/repos/ginuerzh/gost/releases/latest | grep -E "browser_download_url.*gost-linux-amd64" | cut -f4 -d\")"
+fi
 [ -e /opt/gost/ ] || mkdir -p /opt/gost/
 [ -e /opt/gost/config.json ] || wget https://${GITHUB_RAW_URL}/myxuchangbin/shellscript/master/gost/config.json -O /opt/gost/config.json
 [ -e /opt/gost/gost ] && rm -rf /opt/gost/gost
-if [ ${CN} == 1 ]; then
-    URL="$(echo $URL | sed 's/github.com/download.fastgit.org/')"
-fi
 wget -O - $URL | gzip -d > /opt/gost/gost && chmod +x /opt/gost/gost
 
 cat <<EOF > /etc/systemd/system/gost.service
 [Unit]
 Description=Gost server
-After=syslog.target
-After=network.target
+After=network-online.target
+Wants=network-online.target
+
 [Service]
 LimitCORE=infinity
 LimitNOFILE=512000
 LimitNPROC=512000
 Type=simple
+DynamicUser=yes
 StandardOutput=null
 #StandardError=journal
 WorkingDirectory=/opt/gost
 ExecStart=/opt/gost/gost -C /opt/gost/config.json
-ExecReload=/bin/kill -s HUP $MAINPID
-ExecStop=/bin/kill -s TERM $MAINPID
-Restart=always
 User=root
+Restart=always
+RestartSec=5
+
 [Install]
 WantedBy=multi-user.target
 EOF

526c0b8c399556cb60d615e714f06c3f944aef04
myxuchangbin <gg18701605431@gmail.com>
2021-12-30 12:37:39 +0800

    内核参数微调

--- startsys.sh
+++ startsys.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #Uses: 适用于centos7/8 debian ubuntu
-#date: 2021-05-027
+#date: 2021-12-30
+#前仅为个人使用，请谨慎用于生产环境
 
 red='\033[0;31m'
 green='\033[0;32m'
@@ -198,60 +199,98 @@ set_file(){
 
 #sysctl.conf设置
 set_sysctl(){
-if [ `grep -c "#sysctl20210527" /etc/sysctl.conf` -eq 0 ];then
 echo -e "${yellow}优化系统内核参数${plain}"
+sed -i '/net.ipv4.icmp_echo_ignore_broadcasts/d' /etc/sysctl.conf
+sed -i '/net.ipv4.icmp_ignore_bogus_error_responses/d' /etc/sysctl.conf
+sed -i '/net.ipv4.conf.all.rp_filter/d' /etc/sysctl.conf
+sed -i '/net.ipv4.conf.default.rp_filter/d' /etc/sysctl.conf
+sed -i '/net.ipv4.conf.all.accept_source_route/d' /etc/sysctl.conf
+sed -i '/net.ipv4.conf.default.accept_source_route/d' /etc/sysctl.conf
+sed -i '/kernel.sysrq/d' /etc/sysctl.conf
+sed -i '/kernel.core_uses_pid/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_syncookies/d' /etc/sysctl.conf
+sed -i '/kernel.msgmnb/d' /etc/sysctl.conf
+sed -i '/kernel.msgmax/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_max_tw_buckets/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_sack/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_fack/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_window_scaling/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_rmem/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_wmem/d' /etc/sysctl.conf
+sed -i '/net.core.wmem_default/d' /etc/sysctl.conf
+sed -i '/net.core.rmem_default/d' /etc/sysctl.conf
+sed -i '/net.core.rmem_max/d' /etc/sysctl.conf
+sed -i '/net.core.wmem_max/d' /etc/sysctl.conf
+sed -i '/net.ipv4.udp_rmem_min/d' /etc/sysctl.conf
+sed -i '/net.ipv4.udp_wmem_min/d' /etc/sysctl.conf
+sed -i '/net.core.netdev_max_backlog/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_max_syn_backlog/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_timestamps/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_synack_retries/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_syn_retries/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_tw_recycle/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_tw_reuse/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_fin_timeout/d' /etc/sysctl.conf
+sed -i '/net.ipv4.ip_local_port_range/d' /etc/sysctl.conf
+sed -i '/net.ipv4.conf.all.accept_redirects/d' /etc/sysctl.conf
+sed -i '/net.ipv4.conf.default.accept_redirects/d' /etc/sysctl.conf
+sed -i '/net.ipv4.conf.all.secure_redirects/d' /etc/sysctl.conf
+sed -i '/net.ipv4.conf.default.secure_redirects/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_no_metrics_save/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_moderate_rcvbuf/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_retries2/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_slow_start_after_idle/d' /etc/sysctl.conf
+sed -i '/net.ipv4.route.gc_timeout/d' /etc/sysctl.conf
+sed -i '/fs.file-max/d' /etc/sysctl.conf
+sed -i '/fs.inotify.max_user_instances/d' /etc/sysctl.conf
+sed -i '/vm.swappiness/d' /etc/sysctl.conf
+sed -i '/net.core.somaxconn/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_keepalive_time/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_keepalive_probes/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_keepalive_intvl/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_fastopen/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_mtu_probing/d' /etc/sysctl.conf
+sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
 cat << EOF >> /etc/sysctl.conf
-#预防ICMP探测
 net.ipv4.icmp_echo_ignore_broadcasts = 1
 net.ipv4.icmp_ignore_bogus_error_responses = 1
-#对直接连接的网络进行反向路径过滤
 net.ipv4.conf.all.rp_filter = 1
 net.ipv4.conf.default.rp_filter = 1
-#不允许接受含有源路由信息的ip包
 net.ipv4.conf.all.accept_source_route = 0
 net.ipv4.conf.default.accept_source_route = 0
-#关闭sysrq功能
 kernel.sysrq = 0
-#core文件名中添加pid作为扩展名
 kernel.core_uses_pid = 1
-#开启SYN洪水攻击保护
 net.ipv4.tcp_syncookies = 1
-#修改消息队列长度
 kernel.msgmnb = 65535
 kernel.msgmax = 65535
-#timewait的数量，默认180000
 net.ipv4.tcp_max_tw_buckets = 5000
 net.ipv4.tcp_sack = 1
+net.ipv4.tcp_fack = 1
 net.ipv4.tcp_window_scaling = 1
-net.ipv4.tcp_rmem = 4096 87380 67108864
-net.ipv4.tcp_wmem = 4096 87380 67108864
+net.ipv4.tcp_rmem = 4096 87380 16777216
+net.ipv4.tcp_wmem = 4096 65536 16777216
 net.core.wmem_default = 65536
 net.core.rmem_default = 65536
-net.core.rmem_max = 67108864
-net.core.wmem_max = 67108864
-#每个网络接口接收数据包的速率比内核处理这些包的速率快时，允许送到队列的数据包的最大数目
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+net.ipv4.udp_rmem_min=4096
+net.ipv4.udp_wmem_min=4096
 net.core.netdev_max_backlog = 4096
-#未收到客户端确认信息的连接请求的最大值
 net.ipv4.tcp_max_syn_backlog = 4096
 net.ipv4.tcp_timestamps = 0
-#SYN的重试次数，适当降低该值，有助于防范SYN攻击
 net.ipv4.tcp_synack_retries = 2
 net.ipv4.tcp_syn_retries = 2
-#关闭timewait 快速回收
 net.ipv4.tcp_tw_recycle = 0
-#开启重用。
 net.ipv4.tcp_tw_reuse = 1
 net.ipv4.tcp_fin_timeout = 30
-#允许系统打开的端口范围
-net.ipv4.ip_local_port_range = 1024 65000
-# 确保无人能修改路由表
+net.ipv4.ip_local_port_range = 16384 65535
 net.ipv4.conf.all.accept_redirects = 0
 net.ipv4.conf.default.accept_redirects = 0
 net.ipv4.conf.all.secure_redirects = 0
 net.ipv4.conf.default.secure_redirects = 0
-#
 net.ipv4.tcp_no_metrics_save = 1
-#net.ipv4.tcp_moderate_rcvbuf = 1
+net.ipv4.tcp_moderate_rcvbuf = 1
 net.ipv4.tcp_retries2 = 8
 net.ipv4.tcp_slow_start_after_idle = 0
 net.ipv4.route.gc_timeout = 100
@@ -260,16 +299,16 @@ fs.inotify.max_user_instances = 8192
 vm.swappiness = 0
 net.core.somaxconn = 4096
 net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.tcp_keepalive_probes = 5
+net.ipv4.tcp_keepalive_intvl = 30
 net.ipv4.tcp_fastopen = 3
 net.ipv4.tcp_mtu_probing = 1
 net.core.default_qdisc = fq
 net.ipv4.tcp_congestion_control = bbr
-#sysctl20210527
 EOF
-/sbin/sysctl -p /etc/sysctl.conf  
+/sbin/sysctl -p /etc/sysctl.conf
 /sbin/sysctl -w net.ipv4.route.flush=1
 echo -e "${green}完成${plain}"
-fi
 }
 
 #系统熵值优化

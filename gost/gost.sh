883c034c599952e8a854951cf1dfb86241326d02
myxuchangbin <gg18701605431@gmail.com>
2022-11-22 00:16:10 +0800

    更新下载链接，支持指定版本安装

--- gost/gost.sh
+++ gost/gost.sh
@@ -1,22 +1,46 @@
 #!/usr/bin/env bash
 # Wiki: https://v2.gost.run/
 # Usage: bash <(curl -s https://raw.githubusercontent.com/myxuchangbin/shellscript/master/gost/gost.sh)
-# Uninstall: systemctl stop gost; systemctl disable gost; rm -rf /etc/systemd/system/gost.service /opt/gost
+# Uninstall: systemctl disable gost --now ; rm -rf /etc/systemd/system/gost.service /opt/gost
 
 GITHUB_RAW_URL="raw.githubusercontent.com"
 GITHUB_URL="github.com"
 CN=0
-if [ -n "$1" ]; then
-    if echo "$1" | grep -qwi "cn"; then
+# 默认获取最新版本号
+targetversion=$(wget -qO- -t1 -T2 "https://api.github.com/repos/ginuerzh/gost/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/v//g;s/,//g;s/ //g')
+
+# -v 指定版本号
+# -c 国内加速模式
+# -h 帮助信息
+while getopts ":v:ch" optname
+do
+    case "$optname" in
+      "v")
+        targetversion=$OPTARG
+        ;;
+      "c")
         CN=1
-        GITHUB_RAW_URL="raw.fastgit.org"
-        GITHUB_URL="hub.fastgit.org"
-    fi
-fi
+        ;;
+      "h")
+        echo "支持选项: -v 指定版本号 -c 国内加速模式"
+        exit 0
+        ;;
+      ":")
+        echo "选项 $OPTARG 无参数值"
+        exit 1
+        ;;
+      "?")
+        echo "无效选项 $OPTARG"
+        exit 1
+        ;;
+    esac
+done
 if [ ${CN} == 1 ]; then
-    URL="https://ghproxy.com/https://github.com$(wget -qO- "https://ghproxy.com/https://github.com/ginuerzh/gost/releases/latest" | grep -E "href=.*gost-linux-amd64" | awk -F "\"" '{print $2}')"
+    URL="https://ghproxy.com/https://github.com/ginuerzh/gost/releases/download/v${targetversion}/gost-linux-amd64-${targetversion}.gz"
+    GITHUB_RAW_URL="raw.fastgit.org"
+    GITHUB_URL="hub.fastgit.org"
 else
-    URL="$(wget -qO- https://api.github.com/repos/ginuerzh/gost/releases/latest | grep -E "browser_download_url.*gost-linux-amd64" | cut -f4 -d\")"
+    URL="https://github.com/ginuerzh/gost/releases/download/v${targetversion}/gost-linux-amd64-${targetversion}.gz"
 fi
 [ -e /opt/gost/ ] || mkdir -p /opt/gost/
 [ -e /opt/gost/config.json ] || wget https://${GITHUB_RAW_URL}/myxuchangbin/shellscript/master/gost/config.json -O /opt/gost/config.json
@@ -60,10 +84,11 @@ crontab -l > /tmp/gostcronconf
 if grep -wq "gost.service" /tmp/gostcronconf;then
   sed -i "/gost.service/d" /tmp/gostcronconf
 fi
-echo "3 6 * * *  /usr/bin/systemctl restart gost.service" >> /tmp/gostcronconf
+echo "0 6 * * *  /usr/bin/systemctl restart gost.service" >> /tmp/gostcronconf
 crontab /tmp/gostcronconf
 rm -f /tmp/gostcronconf
-echo -e "定时任务设置成功！每天6点3分重启gost服务"
+echo -e "已设置定时：每天6:00重启gost服务以释放内存压力！"
 
 systemctl daemon-reload
-systemctl enable gost.service --now
+systemctl enable gost.service
+systemctl restart gost.service
